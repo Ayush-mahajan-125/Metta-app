eba8ac32108db5d03e28e6d3cc2bcbdfb33d93f3
Ayush-mahajan-125 <am3854335@gmail.com>
2025-03-04 18:43:08 +0530

    worked on navbar fix issue and changes colors

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,65 +1,102 @@
-// File: src/components/Navbar/Navbar.js
-import React from 'react';
-import './Navbar.css';
-import { NavLink } from 'react-router-dom';
-import mettaLogo from '../../assets/img/metta-logo.png';
-
-import { useLanguage } from '../../LanguageContext';
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import mettaLogo from "../../assets/img/metta-logo.png";
+import { useLanguage } from "../../LanguageContext";
+import "./Navbar.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const Navbar = () => {
-
   const { translate } = useLanguage();
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
   return (
-    <nav className="navbar navbar-expand-lg msm-navmneu-bar py-3 fixed-top">
+    <nav className="navbar navbar-expand-lg msm-navmenu-bar py-3 fixed-top bg-white">
       <div className="container">
         <NavLink className="navbar-brand text-success" to="/">
-          <img src={mettaLogo} className="img-fluid" alt="MeTta Financial Investment & Trading Training Institute" loading="lazy" />
+          <img
+            src={mettaLogo}
+            className="img-fluid"
+            alt="MeTta Financial Investment & Trading Training Institute"
+            loading="lazy"
+          />
         </NavLink>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button
+          className="navbar-toggler"
+          type="button"
+          aria-controls="navbarSupportedContent"
+          aria-expanded={!isNavCollapsed}
+          aria-label="Toggle navigation"
+          onClick={handleNavCollapse}
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div
+          className={`collapse navbar-collapse ${isNavCollapsed ? "" : "show"}`}
+          id="navbarSupportedContent"
+        >
           <ul className="navbar-nav ms-auto text-center text-white">
-
-            {/* Home */}
             <li className="nav-item">
-              <NavLink className="nav-link" to="/" aria-current="page"> {translate('Home')}</NavLink>
-            </li>
-            {/* /Home */}
-
-            {/* Course Modules */}
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/course-modules" aria-current="page">
-                Course Modules
+              <NavLink
+                className="nav-link"
+                to="/"
+                onClick={() => setIsNavCollapsed(true)}
+              >
+                {translate("Home")}
               </NavLink>
             </li>
-            {/* /Course Modules */}
 
-            {/* About Us */}
             <li className="nav-item">
-              <NavLink className="nav-link" to="/about-us">{translate('About us')}</NavLink>
+              <NavLink
+                className="nav-link"
+                to="/course-modules"
+                onClick={() => setIsNavCollapsed(true)}
+              >
+                {translate("Course Modules")}
+              </NavLink>
             </li>
-            {/* /About Us */}
 
-            {/* Contact us */}
-            <li className="nav-item ">
-              <NavLink className="nav-link" to="/contact-us">{translate('Contact us')}</NavLink>
-            </li>
-            {/* /Contact us */}
-
-            {/* Photo Gallery */}
             <li className="nav-item">
-              <NavLink className="nav-link" to="/testimonial">{translate('Testimonial')}</NavLink>
+              <NavLink
+                className="nav-link"
+                to="/about-us"
+                onClick={() => setIsNavCollapsed(true)}
+              >
+                {translate("About us")}
+              </NavLink>
             </li>
-            {/* Photo Gallery */}
 
-            {/* Testimonials */}
-            <li className="nav-item ">
-              <NavLink className="nav-link" to="/gallery">{translate('Gallery')}</NavLink>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                to="/contact-us"
+                onClick={() => setIsNavCollapsed(true)}
+              >
+                {translate("Contact us")}
+              </NavLink>
             </li>
-            {/* /Testimonials */}
 
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                to="/testimonial"
+                onClick={() => setIsNavCollapsed(true)}
+              >
+                {translate("Testimonial")}
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                to="/gallery"
+                onClick={() => setIsNavCollapsed(true)}
+              >
+                {translate("Gallery")}
+              </NavLink>
+            </li>
           </ul>
         </div>
       </div>
